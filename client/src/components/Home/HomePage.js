992a9748523c2cfbb6c9d341e672e405907de187
monazir222 <monaziralamdavps@gmail.com>
2023-09-09 21:42:39 +0530

    Enhanced Design

--- client/src/components/Home/HomePage.js
+++ client/src/components/Home/HomePage.js
@@ -5,7 +5,7 @@ import MuiDrawer from '@mui/material/Drawer';
 import MuiAppBar from '@mui/material/AppBar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import { Card, Paper, CardMedia, Grid, CardContent, CardHeader, CardActions, Collapse, Avatar, Hidden } from '@mui/material'
+import { Card, Paper, CardMedia, Grid, CardContent, CardHeader, CardActions, Collapse, Avatar, Hidden, Box } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import './Home.css'
 import hotel from '../../assets/images/hotel.jpg';
@@ -46,31 +46,47 @@ function HomePage() {
             >
                 <Card
                     sx={{
+                        backgroundColor: "#111",
+                        boxShadow: 5,
+                        // border:"1px solid #7FDBFF",
                         position: "relative",
-                        // width: {
-                        //     sx: 1.0, // 100%
-                        //     sm: 500,
-                        //     md: 1000,
-                        // },
+                        width: {
+                            sx: 1.0, // 100%
+                            sm: 500,
+                            md: 1000,
+                        },
                         width: "800px"
                     }}>
-                    <CardMedia
-                        component="img"
-                        height="278"
-                        image={hotel}
-                        alt="Paella dish"
-                    />
-                    <div className='cardTextOverlay'>
-                        Students
-                    </div>
+                    <Box sx={{ position: 'relative' }}>
+                        <CardMedia
+                            component="img"
+                            height="150"
+                            image={hotel}
+                            alt="Paella dish"
+                        />
+                        <Box
+                            sx={{
+                                position: 'absolute',
+                                bottom: 0,
+                                left: 0,
+                                width: '100%',
+                                bgcolor: 'rgba(0, 0, 0, 0.54)',
+                                color: 'white',
+                                padding: '10px',
+                            }}
+                        >
+                            <Typography variant="h5">STUDENTS</Typography>
+                            {/* <Typography variant="body2" style={{paddingRight:"10px"}}>"A good teacher can awaken joy in their students and leave a positive impression that lasts a lifetime...."</Typography> */}
+                        </Box>
+                    </Box>
                     <CardActions disableSpacing>
                         <Grid container>
-                            <Grid xs={11} >
-                                <Button className='styleButtonCard' ><span>View</span></Button>
-                                <Button className='styleButtonCard buttonPadding' ><span>Input</span></Button>
-                                <Button className='styleButtonCard buttonPadding' ><span>Manage</span></Button>
+                            <Grid xs={11} className="btnContainer" >
+                                <Button className='styleButtonCard' ><span>VIEW</span></Button>
+                                <Button className='styleButtonCard buttonPadding' ><span>INPUT</span></Button>
+                                <Button className='styleButtonCard buttonPadding' ><span>MANAGE</span></Button>
                             </Grid>
-                            <Grid xs={1}>
+                            <Grid xs={1} className="arrowDown">
                                 <ExpandMore
                                     expand={expanded}
                                     onClick={handleExpandClick}
@@ -84,15 +100,15 @@ function HomePage() {
                     </CardActions>
                     <Collapse in={expanded} timeout="auto" unmountOnExit>
                         <CardContent>
-                            <Typography paragraph>Here you can explore the Students registerd with us.</Typography>
-                            <Typography paragraph>
+                            <Typography paragraph className='cardTypography'>Here you can explore the Students registerd with us.</Typography>
+                            <Typography paragraph className='cardTypography'>
                                 Details like Student's address , class , age and course can be seen.
                             </Typography>
                         </CardContent>
                     </Collapse>
                 </Card>
             </Grid>
-                    </Grid>
+        </Grid >
         // </Box>
     )
 }
