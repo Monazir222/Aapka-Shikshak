f259301a5f83833ae2bb1fb81f9d893477ca50a3
monazir222 <monaziralamdavps@gmail.com>
2023-09-19 14:41:04 +0530

    added controllers routes and added MongoDB with the help of mongoose

--- ServerAPI/index.js
+++ ServerAPI/index.js
@@ -1,22 +1,28 @@
-import express from "express";
-import cors from "cors";
+require("dotenv").config();
 
-// const express = require("express");
+const express = require("express");
 const app = express();
+const connectDB = require("./db/connect");
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 const PORT = process.env.PORT || 5000;
+
+const products_routes = require("./routes/products")
 
 app.get("/", (req, res) => {
      res.send("aapka shikshak !");
 });
 
+//middleware or To set routes
+app.use("/api/products", products_routes )
+
 const start = async () => {
     try {
-        app.listen(PORT, () => {
-            console.log(`${PORT} Connected`);
+     await connectDB(process.env.MONGODB_URL);
+     app.listen(PORT, () => {
+     console.log(`${PORT} Connected`);
         })
     } catch (error) {
         console.log(error);
