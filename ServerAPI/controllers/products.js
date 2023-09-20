f675b4d0cc7e87d3a1d889f9ed4f3b11f03fe5d1
monazir222 <monaziralamdavps@gmail.com>
2023-09-20 15:10:13 +0530

    Added sort and select functionalities to our Api in Product Data

--- ServerAPI/controllers/products.js
+++ ServerAPI/controllers/products.js
@@ -1,9 +1,44 @@
+const Product = require("../models/products");
+
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: "I am getAllProducts" });
+    const { company, name, feature, sort, select } = req.query;
+    const queryObject = {};
+
+    if (company) {
+        queryObject.company = company;
+    }
+
+    if (feature) {
+        queryObject.feature = feature;
+    }
+
+    if (name) {
+        queryObject.name = {$regex: name, $options: "i"};
+    }
+
+    let apiData = Product.find(queryObject);
+
+    if (sort) {
+        // let sortFix = select.replace("," , " ");
+        let sortFix = select.split(",").join(" ");
+        apiData = apiData.sort(sortFix);
+    }
+
+    if (select) {
+        // let sortFix = select.replace("," , " ");
+        let sortFix = select.split(",").join(" ");
+        apiData = apiData.select(sortFix);
+    }
+
+    const myData = await apiData;  
+    console.log(myData );
+    res.status(200).json({ myData });
 };
 
 const getAllProductsTesting = async (req, res) => {
-    res.status(200).json({ msg: "I am getAllProductsTesting" });
+    const myData = await Product.find(req.query).select("name"); 
+    console.log(req.query);
+    res.status(200).json({ myData });
 };
 
 module.exports = {getAllProducts, getAllProductsTesting};
